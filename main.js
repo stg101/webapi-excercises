95ac8fceac0e00cec6a94a769a506eb2beee74c0
Ricardo <ry.yrupailla@gmail.com>
2019-05-21 16:25:03 -0500

    Add storage api data

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
-let promise = fetch("https://got-quotes.herokuapp.com/quotes");
-promise.then(response => response.json()).then(data => console.log(data));
+function updateDOM(elementId, data) {
+  let $quote = document.querySelector(`#${elementId} .quote`);
+  let $character = document.querySelector(`#${elementId} .character`);
+  $quote.innerText = data.quote;
+  $character.innerText = data.character;
+}
 
-// .then(function(data) {
-//   let $content = document.getElementById("content");
-//   $content.innerText = data;
-// });
+function conversion(data) {
+  updateDOM("api", data);
+  localStorage.setItem("dataStorage", JSON.stringify(data));
+}
+
+function showStorage() {
+  if (localStorage.length > 0) {
+    let dataStorage = localStorage.getItem("dataStorage");
+    dataStorage = JSON.parse(dataStorage);
+    updateDOM("storage", dataStorage);
+  }
+}
+
+let promise = fetch("https://got-quotes.herokuapp.com/quotes");
+promise.then(response => response.json()).then(data => conversion(data));
+
+showStorage();
